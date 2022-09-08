18cdb2c508bc16371bf605e3aaa7818e326bc7aa
onurozrk <112889454+onurozrk@users.noreply.github.com>
2022-09-08 20:23:31 +0300

    onur öztürk
    ödevler

---
+++ ödevler/Ödev 1.py
@@ -0,0 +1,8 @@
+sayi = 0
+while (sayi < 1000):
+	if sayi % 2 == 0:
+		print(f"çift sayı: {sayi}")
+	else:
+		sayi % 2 == 1
+		print(f"Tek sayı: {sayi}")
+	sayi +=1
---
+++ ödevler/Ödev 2.py
@@ -0,0 +1,10 @@
+baslangic = 1
+bitis = int(input("Lütfen sayı giriniz: "))
+while (baslangic < bitis):
+	if baslangic % 2 == 0:
+		print(baslangic **2)
+	else:
+		baslangic % 2 == 1
+		print(baslangic ** 3)
+	baslangic +=1
+
